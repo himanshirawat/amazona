9ab3421a4b14ff0684286d883279c82c833e8a7f
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2022-11-04 22:21:06 +0530

    Home and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
+    <Router>
     <div>
       <header>
-        <a href = '/'>amazona</a>
+        <Link to = '/'>amazona</Link>
       </header>
-      <main>list products</main>
+      <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </main>
     </div>
+    </Router>
   );
 }
 
