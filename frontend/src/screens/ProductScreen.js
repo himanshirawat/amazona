5a7f4c71ea555755120cbf200192697a889b64e6
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2022-11-08 20:18:21 +0530

    Upadated add to cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -45,21 +45,21 @@ function ProductScreen(){
         fetchData();
     },[slug]);
 
-    const {state, dispatch: ctxDispatch} = useContext(Store);
-    const {cart} = state;
+    const {dispatch: ctxDispatch} = useContext(Store);
+    // const {cart} = state;
     const addToCartHandler = async() => {
-      const existItem = cart.cartItems.find((x) => x._id === product._id);
-      const quantity = existItem ? existItem.quantity + 1 : 1;
-      const {data} = await axios.get(`/api/products/${product._id}`);
-      if(data.countInStock < quantity) {
-        window.alert('sorry.product is out of stock');
-        return;
-      }
+      // const existItem = cart.cartItems.find((x) => x._id === product._id);
+      // const quantity = existItem ? existItem.quantity + 1 : 1;
+      // const {data} = await axios.get(`/api/products/${product._id}`);
+      // if(data.countInStock < quantity) {
+      //   window.alert('sorry.product is out of stock');
+      //   return;
+      // }
       ctxDispatch({
         type:'CART_ADD_ITEM',
-        payload: {...product,quantity:1}})
-      
-    }
+        payload: {...product,quantity : 1},
+      });
+    };
     return loading ? (
       <LoadingBox />
       ) : error ? (
