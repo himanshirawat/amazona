ef278d8f25e5157de52c4bbac664e8ca93da59a9
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2022-12-23 21:17:24 +0530

    upto search box layout completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes,Route,Link,} from "react-router-dom";
-import {ToastContainer } from 'react-toastify';
+import {toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import {Navbar,Container, Nav, Badge, NavDropdown} from "react-bootstrap";
+import {Navbar,Container, Nav, Badge, NavDropdown, Button} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import { Store } from "./Store";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
@@ -16,6 +16,9 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import { getError } from "./utils";
+import axios from "axios";
+import SearchBox from "./component/SearchBox";
 
 function App() {
   const { state,dispatch:ctxDispatch } = useContext(Store);
@@ -27,18 +30,37 @@ function App() {
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
     window.location.href = '/signin';
-  }
+  };
+
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try{
+        const {data} = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch(err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  },[])
 
   return (
     <Router>
-    <div className='d-flex flex-column site-container'>
+    <div className= {sidebarIsOpen ? 'd-flex flex-column site-container active-cont' : 'd-flex flex-column site-container'} >
       <ToastContainer position="bottom-center" limit={1} ></ToastContainer>
       <header> 
         <Navbar bg="dark" variant="dark" expand="lg" >
           <Container >
+            <Button variant="dark" onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
+              <i className="fas fa-bars"></i>
+            </Button>
             <LinkContainer to = "/"><Navbar.Brand>ecommerce</Navbar.Brand></LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
+              <SearchBox />
             <Nav className="me-auto w-100 justify-content-end">
               <Link to="/cart" className="nav-link">
                 Cart
@@ -68,6 +90,20 @@ function App() {
         </Navbar>
 
       </header>
+      <div className= {sidebarIsOpen ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column' : 'side-navbar d-flex justify-content-between flex-wrap flex-column'}>
+        <Nav className="flex-column text-white w-100 p-2">
+          <Nav.Item>
+            <strong>Categories</strong>
+          </Nav.Item>
+          {categories.map((category) => (
+            <Nav.Item key={category}>
+              <Link to={`/search?category=${category}`} onClick={() => setSidebarIsOpen(false)}>
+                <Nav.Link>{category}</Nav.Link>
+              </Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+      </div>
       <main>
         <Container className="mt-3">
         <Routes>
