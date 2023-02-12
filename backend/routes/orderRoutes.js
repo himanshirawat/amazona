58b8e0551d92c643cb830e4d4b3eb574b8fa1270
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2023-02-12 21:20:35 +0530

    listing if orders and deliver order option are implemnted successfully

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -98,4 +98,20 @@ if (order) {
 })
 );
 
+orderRouter.put(
+  '/:id/deliver',
+  isAuth,
+  expressAsyncHandler(async (req,res)=> {
+    const order = await Order.findById(req.params.id);
+    if(order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      await order.save();
+      res.send({message:'Oredr Delivered'});
+    } else {
+      res.status(404).send({message:'Order Not Found'});
+    }
+  })
+)
+
 export default orderRouter;
