f4025196b9b64f09ff1f09e635bc82f9843c298a
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2023-02-17 21:22:45 +0530

    REview option completed successfully

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -78,7 +78,7 @@ productRouter.post(
     const productId = req.params.id;
     const product = await Product.findById(productId);
     if (product) {
-      if (product.numReviews.find((x) => x.name === req.user.name)) {
+      if (product.reviews.find((x) => x.name === req.user.name)) {
         return res 
          .status(400)
          .send({message:'You already submitted a review'});
