1597c28c5b8917323d820f08d4f127865d591400
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2023-03-03 23:53:33 +0530

    class to className

--- frontend/src/footer/footer.js
+++ frontend/src/footer/footer.js
@@ -4,16 +4,16 @@ import "./footer.css";
 const Footer = () => {
     return (
         <footer id="footer">
-            <div class="leftfooter ">
+            <div className="leftfooter ">
                 <h4>About Us</h4>
                 <p>We are the Final Year Students of Institute of Technology Gopeshwar pursuing Computer science Engineering. This Website is about Online Ecommerce System represents our Web development skills. </p>
             </div>  
-            <div class="midfooter">
+            <div className="midfooter">
                 <h1>ecommerce</h1>
                 <p>High Quality is our first priority</p>
                 <p>Copyrights 2023 &copy; ecommerce</p>
             </div> 
-            <div class="rightfooter">
+            <div className="rightfooter">
                  <h4>Follow us</h4>
                  <a href="https://www.linkedin.com/in/himanshi-rawat">Himanshi Rawat</a>
                  <a href="https://www.linkedin.com/in/himanshi-rawat">Atul Paswan</a>
