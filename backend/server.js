89f6d7b0036cffd543ba90e9601c3118ad7c016c
Himanshika Rawat <69066711+himanshirawat@users.noreply.github.com>
2023-03-04 19:41:02 +0530

    localhost change to render link

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,6 @@ app.use((err,req,res,next)=>{
 const port = process.env.PORT || 5000;
 app.listen(port,()=>
 {
-    console.log(`server at  http://localhost:${port}`);
+    console.log(`server at "https://amazona-clone.onrender.com"`);
 })
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -34,7 +34,7 @@ function HomeScreen(){
     const fetchData = async () => {
       dispatch({type: 'FETCH_REQUEST'})
       try {
-        const result = await axios.get('/https://amazona-clone.onrender.com/products');
+        const result = await axios.get('/api/products');
         dispatch({type:'FETCH_SUCCESS',payload: result.data});
       } catch (err) {
         dispatch({type:'FETCH_FAILED',payload: err.message});
